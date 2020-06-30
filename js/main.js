36151430de01f0854b656cef71374117b675e37e
Arya Ashari <aryaashari100@gmail.com>
2020-06-30 10:58:14 +0700

    menambahkan bagian testimoni

--- js/main.js
+++ js/main.js
@@ -15,4 +15,24 @@ $(document).ready(function () {
         
     });
 
+
+    // Owl Carousel
+    $('.owl-carousel').owlCarousel({
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 2
+            }
+        }
+    });
+
 });
